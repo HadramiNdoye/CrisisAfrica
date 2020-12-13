b9d62b62a57d94c41ff29a60c54db049b6b1790d
IsmailRamde <ismaramde@gmail.com>
2020-12-13 13:30:08 +0100

    Initial commit

--- R/Correlation.R
+++ R/Correlation.R
@@ -9,6 +9,8 @@
 Correlation <- function(data){
   dtfc <- data[4:14]
   corr <- cor(dtfc,method="pearson")
-  c <- corrplot(corr, type="upper", order="hclust", tl.col="black", tl.srt=45)
+  c <- corrplot(corr, type="upper", order="hclust", tl.col="black", tl.srt=45,
+                title="Matrice de corrélation (corrélogramme)", mar=c(0,0,5,0),
+                tl.offset = 1)
   return(c)
 }
